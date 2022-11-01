1b35024f9c7b073ac8a1b13104535979f9289ad3
Rafael Felipe <102329554+Martinez0722@users.noreply.github.com>
2022-11-01 16:36:12 -0300

    padronizando a aplicação

---
+++ react-base/base/src/pages/Login/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container } from '../../styles/GlobalStyles';
+import { Title, Paragraph } from './styled';
+
+export default function Login() {
+  return (
+    <Container>
+      <Title>
+        Login
+        <small>salut</small>
+      </Title>
+      <Paragraph>Lorem</Paragraph>
+    </Container>
+  );
+}
