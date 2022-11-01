724707602b3aecc611ee95a9a6bcddd59605cbe9
Rafael Felipe <102329554+Martinez0722@users.noreply.github.com>
2022-11-01 15:38:37 -0300

    Limpando o projeto

--- react-base/base/.eslintrc.js
+++ react-base/base/.eslintrc.js
@@ -3,30 +3,25 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'prettier',
-    'prettier/react',
-  ],
-  overrides: [
-  ],
-  parser:'babel-eslint',
+  extends: ['plugin:prettier/recommended'],
+  parser: '@babel/eslint-parser',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
     ecmaVersion: 'latest',
+    requireConfigFile: false,
     sourceType: 'module',
   },
-  plugins: [
-    'prettier',
-    'react',
-    'react-hooks'
-
-  ],
+  plugins: ['react', 'prettier', 'react-hooks'],
   rules: {
-    'prettier/prettier': 'error',
-    'react/jsx-filename-extension':0,
-    'import/prefer-default-export':0,
-    'react-hooks/rules-of-hooks':'error',
-    'react-hooks/exhaustive': 'warn'
+    'prettier/prettier': 2,
+    'react/jsx-filename-extension': 0,
+    'import/prefer-default-export': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
