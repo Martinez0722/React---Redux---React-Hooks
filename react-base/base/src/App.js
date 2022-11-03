0f7e9ce0451c82285481c028cc2e357fc84a321d
Rafael Felipe <102329554+Martinez0722@users.noreply.github.com>
2022-11-03 19:54:42 -0300

    Adicionando rotas

--- react-base/base/src/App.js
+++ react-base/base/src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Login from './pages/Login';
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Header />
-      <Login />
-      <GlobalStyles />
+      <BrowserRouter>
+        <Header />
+        <Login />
+        <GlobalStyles />
+      </BrowserRouter>
     </>
   );
 }
