75e66151dd2438630de8d8fa69632d933990573c
Rafael Felipe <102329554+Martinez0722@users.noreply.github.com>
2022-11-03 21:27:33 -0300

    Paginas fechadas

--- react-base/base/src/pages/Page404/index.js
+++ react-base/base/src/pages/Page404/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import history from '../../services/history';
+import { Container } from '../../styles/GlobalStyles';
 
 export default function Page404() {
   history.push('/');
-  return <h1>Essa página não existe</h1>;
+  return (
+    <Container>
+      <h1>Essa página não existe</h1>
+    </Container>
+  );
 }
