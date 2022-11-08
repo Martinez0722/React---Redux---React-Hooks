63ad4826053d88b4ae3fc3e9244dc6dc5b813fce
Rafael Felipe <102329554+Martinez0722@users.noreply.github.com>
2022-11-08 17:20:18 -0300

    dasvasf

--- react-base/base/src/store/index.js
+++ react-base/base/src/store/index.js
@@ -1,5 +1,8 @@
+import { persistStore } from 'redux-persist';
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+
+import persistedReducers from './modules/reduxPersist';
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
@@ -10,4 +13,5 @@ const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 
 sagaMiddleware.run(rootSaga);
 
+export const persistor = persistStore(store);
 export default store;
