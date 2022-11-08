d900e181b88762896d90090ffab1798360beb5df
Rafael Felipe <102329554+Martinez0722@users.noreply.github.com>
2022-11-08 09:53:50 -0300

    REDUX

--- react-base/base/src/store/modules/example/reducer.js
+++ react-base/base/src/store/modules/example/reducer.js
@@ -1,10 +1,22 @@
+import * as types from '../types';
+
 const initialState = {
   botaoClicado: false,
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
-    case 'BOTÃO CLICADO': {
+    case 'BOTAO_CLICADO_SUCCESS': {
+      const newState = { ...state };
+      newState.botaoClicado = !newState.botaoClicado;
+      return newState;
+    }
+    case 'BOTAO_CLICADO_FAILURE': {
+      const newState = { ...state };
+      newState.botaoClicado = !newState.botaoClicado;
+      return newState;
+    }
+    case 'BOTAO_CLICADO_REQUEST': {
       const newState = { ...state };
       newState.botaoClicado = !newState.botaoClicado;
       return newState;
